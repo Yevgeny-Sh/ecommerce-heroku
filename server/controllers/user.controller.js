8c3da5dddd12ba46c56a4ed5d9b9eb6b2e644bcd
Yevgeny-Sh <yevgst7@gmail.com>
2022-01-24 11:15:55 +0200

    update modifies to hide password

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -65,10 +65,11 @@ const updateUser = async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    //inclused save to hash passwords on update
+    await user.save();
     if (!user) {
       return res.status(400).send(`User not found - ${req.params.id}`);
     }
